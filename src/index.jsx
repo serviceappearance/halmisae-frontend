c73450f3a244cf1c3d02d377a302a860bdffed5f
serviceapperance <sebyoug2647@gmail.com>
2024-07-16 17:47:26 +0900

    공통 요소들

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import MainPage from "./components/main/MainPage";
 import MyPage from "./components/mypage/Mypage";
 import ReservationCheckPage from "./components/reservationCheck/ReservationCheckPage";
-import SalePage from "./components/sale/SalePage";
+import PaymentInfoModal from "./components/sale/BargainSale/PaymentInfoModal";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +11,6 @@ root.render(
     {/* <MainPage />
     <ReservationCheckPage />
     <MyPage /> */}
-    <SalePage />
+    <PaymentInfoModal />
   </React.StrictMode>
 );
