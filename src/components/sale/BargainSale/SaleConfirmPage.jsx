17d04bd9b341bff945befd27926409756a3ea3e9
serviceapperance <sebyoug2647@gmail.com>
2024-07-17 17:52:47 +0900

    마감할인 구매 확인 페이지

--- src/components/sale/BargainSale/SaleConfirmPage.jsx
+++ src/components/sale/BargainSale/SaleConfirmPage.jsx
@@ -1,3 +1,28 @@
+import { ReactComponent as BagIcon } from "../../../assets/icons/shopping-bag-big.svg";
 export default function SaleConfirmPage() {
-  return <div></div>;
+  return (
+    <div className="style-page">
+      <div style={iconStyle}>
+        <BagIcon />
+      </div>
+      <div style={titleStyle} className="font-sale-confirm-title">
+        감사합니다!
+      </div>
+      <div className="font-sale-confirm-content">
+        방금 여러분은
+        <br />
+        식사의 낭비를 줄였습니다
+      </div>
+    </div>
+  );
 }
+
+const iconStyle = {
+  display: "grid",
+  placeItems: "center",
+  margin: "102px 53px 38px 53px",
+};
+
+const titleStyle = {
+  margin: "0 0 14px 0",
+};
