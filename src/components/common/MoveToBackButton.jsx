6a049f3f0bafa17d90ea94c2b6c34e6e4eedb2eb
serviceapperance <sebyoug2647@gmail.com>
2024-07-26 12:35:16 +0900

    공통 요소

--- src/components/common/MoveToBackButton.jsx
+++ src/components/common/MoveToBackButton.jsx
@@ -3,11 +3,9 @@ import { ReactComponent as BackIcon } from "../../assets/icons/move-to-back.svg"
 import { useState } from "react";
 
 export default function MoveToBackButton() {
-  const [animationType, setAnimationType] = useState("slide");
   const navigate = useNavigate();
 
   const handleBack = () => {
-    setAnimationType("slide-right");
     navigate(-1);
   };
   return (
